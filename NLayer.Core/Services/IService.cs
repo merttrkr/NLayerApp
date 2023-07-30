f177fd17397eadb626fb1b92455bf88c47e55f9f
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-07-30 20:35:45 +0300

    Add IService

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        //queries does not go to database directly for more performance. It only goes when you call .ToListAsync()
+        // productRepository.where(x=>x.id>5).OrderBy.ToListAsync()
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);// it gets the entity x and returns the result of the operation as boolean x.id>5
+        Task<T> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);// it only changes the entities state in efcore so it does not have async 
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+
+    }
+}
