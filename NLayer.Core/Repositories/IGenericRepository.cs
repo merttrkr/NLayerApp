dd49f039240a3b08fb29c8b057d57fe5bcd74062
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-07-30 20:35:58 +0300

    Update repository

--- NLayer.Core/Repositories/IGenericRepository.cs
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -8,14 +8,14 @@ namespace NLayer.Core.Repositories
 
         //queries does not go to database directly for more performance. It only goes when you call .ToListAsync()
         // productRepository.where(x=>x.id>5).OrderBy.ToListAsync()
-        IQueryable<T> Where(Expression<Func<T,bool>> expression);// it gets the entity x and returns the result of the operation as boolean x.id>5
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);// it gets the entity x and returns the result of the operation as boolean x.id>5
         IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
         Task<T> AnyAsync(Expression<Func<T, bool>> expression);
         Task AddAsync(T entity);
         Task AddRangeAsync(IEnumerable<T> entities);
         void Update(T entity);// it only changes the entities state in efcore so it does not have async 
         void Remove(T entity);
-        void RemoveRange (IEnumerable<T> entities);
+        void RemoveRange(IEnumerable<T> entities);
 
     }
 }
