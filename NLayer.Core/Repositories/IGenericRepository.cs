aa6d39d079fbf4bfe0d6faa6b740265377a33b53
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-07-30 20:02:54 +0300

    Add generic repository

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        //queries does not go to database directly for more performance. It only goes when you call .ToListAsync()
+        // productRepository.where(x=>x.id>5).OrderBy.ToListAsync()
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);// it gets the entity x and returns the result of the operation as boolean x.id>5
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        Task<T> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);// it only changes the entities state in efcore so it does not have async 
+        void Remove(T entity);
+        void RemoveRange (IEnumerable<T> entities);
+
+    }
+}
