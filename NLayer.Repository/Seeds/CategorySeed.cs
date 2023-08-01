e09bb99d67c6521d282bd2554e8ed9091555fa26
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-01 22:51:46 +0300

    Add seed data for testing

---
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(new Category { Id = 1, Name = "kalemler" },
+                new Category { Id = 2, Name = "kitaplar" },
+                new Category { Id = 3, Name = "çantalar" },
+                new Category { Id = 4, Name = "defterler" },
+                new Category { Id = 5, Name = "kıyafetler" });
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    CreatedDate = DateTime.Now,
+                    Price = 100,
+                    Stock= 10,
+                    Name = "kalem 1",
+
+                },
+                 new Product
+                 {
+                     Id = 2,
+                     CategoryId = 1,
+                     CreatedDate = DateTime.Now,
+                     Price = 200,
+                     Stock = 20,
+                     Name = "kalem 2",
+
+                 },
+                  new Product
+                  {
+                      Id = 3,
+                      CategoryId = 1,
+                      CreatedDate = DateTime.Now,
+                      Price = 200,
+                      Stock = 30,
+                      Name = "kalem 3",
+                  },
+                  new Product
+                  {
+                      Id = 4,
+                      CategoryId = 2,
+                      CreatedDate = DateTime.Now,
+                      Price = 200,
+                      Stock = 30,
+                      Name = "kitap 1",
+                  },
+                  new Product
+                  {
+                      Id = 5,
+                      CategoryId = 2,
+                      CreatedDate = DateTime.Now,
+                      Price = 200,
+                      Stock = 30,
+                      Name = "kitap 2",
+                  }
+               );
+        }
+    }
+}
