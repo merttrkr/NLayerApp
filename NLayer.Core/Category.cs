cc79c6074da8ebd178a95feee3630e205fb555f7
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-01 12:17:37 +0300

    Update Category

--- NLayer.Core/Category.cs
+++ NLayer.Core/Category.cs
@@ -8,7 +8,7 @@ namespace NLayer.Core
 {
     public class Category:BaseEntity
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         public ICollection<Product>? Products { get; set; }
     }
