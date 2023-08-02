58a89b1b64a883d8570f10449bbb68878fbc1045
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-02 12:53:58 +0300

    Fix scope

--- NLayer.Core/UnitOfWorks/IUnitOfWork.cs
+++ NLayer.Core/UnitOfWorks/IUnitOfWork.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NLayer.Core.UnitOfWorks
 {
-    internal interface IUnitOfWork
+    public interface IUnitOfWork
     {
         Task CommitAsync();
         void Commit();
