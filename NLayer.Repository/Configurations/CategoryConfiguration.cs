9cfe5fa3b6d08d4e526ba1599b3bb1f3c69b9363
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-02 16:37:04 +0300

    Update Configuration

--- NLayer.Repository/Configurations/CategoryConfiguration.cs
+++ NLayer.Repository/Configurations/CategoryConfiguration.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NLayer.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace NLayer.Repository.Configurations
 {
@@ -15,7 +11,9 @@ namespace NLayer.Repository.Configurations
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
-            builder.Property(x=>x.Name).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
         }
     }
 }
--- NLayer.Repository/Configurations/ProductFeatureConfiguration.cs
+++ NLayer.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -1,22 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NLayer.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NLayer.Repository.Configurations
 {
     public class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
-
     {
         public void Configure(EntityTypeBuilder<ProductFeature> builder)
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
-            builder.HasOne(x => x.Product).WithOne().HasForeignKey<ProductFeature>(x => x.Id);
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
         }
     }
 }
