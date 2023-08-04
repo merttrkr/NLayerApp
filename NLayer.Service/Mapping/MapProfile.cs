7c25e9cd164d3b6b2670ec32d71e1b850006f046
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-04 11:15:50 +0300

    Add comment

---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile : Profile   //if there is too many entity you can create specific profile MapProductProfile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}
