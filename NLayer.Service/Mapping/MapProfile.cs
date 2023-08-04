012570a85ca9241b7052ab47d7cf290a0e0d7b94
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-04 15:23:07 +0300

    Add ProductWithCategoryDto mapping

--- NLayer.Service/Mapping/MapProfile.cs
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -17,6 +17,7 @@ namespace NLayer.Service.Mapping
             CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
             CreateMap<Category, CategoryDto>().ReverseMap();
             CreateMap<ProductUpdateDto, Product>();
+            CreateMap<Product, ProductWithCategoryDto>();
         }
     }
 }
