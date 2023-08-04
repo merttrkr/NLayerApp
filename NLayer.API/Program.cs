331e882efc791fa26dd67deed12fcd2c6072929d
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-04 11:15:05 +0300

    Update Program.cs inject mapping and service

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Repositories;
+using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
 using NLayer.Repository;
 using NLayer.Repository.Repositiories;
 using NLayer.Repository.UnitOfWorks;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
+
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,7 +21,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-//builder.Services.AddScoped(typeof(IService<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
