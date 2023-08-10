2675bbbcd2770c9c2ecd3cb820393f86c8570879
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-08-10 15:50:46 +0300

    Update controller

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -6,8 +6,7 @@ using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+
     public class ProductsController : CustomBaseController
     {
         private readonly IMapper _mapper;
